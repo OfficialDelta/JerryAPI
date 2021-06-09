17b57020bf90a021e7ab48856bd7d51f73796fa6
TSLARoadster <alanalwakeel@hotmail.com>
2021-06-09 11:03:23 -0400

    added price endpoint

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ let app = express()
 
 app.get('/scammer', (req, res) => {
     let clientIP =
-            req.headers['cf-connecting-ip'] ||
-            req.headers['x-forwarded-for'] ||
-            req.connection.remoteAddress
-    console.log(`Request to /scammer/ inbound from ${clientIP} with query ${JSON.stringify(req.query)}`)
+        req.headers['cf-connecting-ip'] ||
+        req.headers['x-forwarded-for'] ||
+        req.connection.remoteAddress
+    console.log(`Request to /scammer inbound from ${clientIP} with query ${JSON.stringify(req.query)}`)
     let scammerJSON = require('./scammer.json')
-	if (req.query.uuid) {
-        let uuid = req.query.uuid.split('-').join('')
+    if (req.query.uuid) {
+        let uuid = req.query.uuid.split('-').join('').toLowerCase()
         if (scammerJSON[uuid]) {
             scammerJSON[uuid].success = true
             res.send(scammerJSON[uuid])
@@ -29,8 +29,34 @@ app.get('/scammer', (req, res) => {
     }
 })
 
+app.get('/price', (req, res) => {
+    let clientIP =
+        req.headers['cf-connecting-ip'] ||
+        req.headers['x-forwarded-for'] ||
+        req.connection.remoteAddress
+    console.log(`Request to /price inbound from ${clientIP} with query ${JSON.stringify(req.query)}`)
+    let priceJSON = require('./data.json')
+    if (req.query.item) {
+        let item = req.query.item
+        if (priceJSON[item]) {
+            priceJSON[item].success = true
+            res.send(priceJSON[item])
+        }
+        else {
+            res.send({
+                success: false,
+                reason: 'Inputted item is not a valid item ID.'
+            })
+        }
+    }
+    else {
+        priceJSON.success = true
+        res.send(priceJSON)
+    }
+})
+
 app.listen(10000, () => {
-	console.log('Scammer API is listening on port 10000')
+    console.log('Scammer API is listening on port 10000')
 })
 
 setInterval(() => {
@@ -45,4 +71,15 @@ setInterval(() => {
         }
         console.log(`stdout: ${stdout}`)
     })
-}, 60*1000)
+    exec('curl -L -O raw.githubusercontent.com/skyblockz/pricecheckbot/master/data.json', (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`)
+            return
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`)
+            return
+        }
+        console.log(`stdout: ${stdout}`)
+    })
+}, 60 * 1000)
